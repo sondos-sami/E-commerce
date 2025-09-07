602327a5bf04252dacc13fb664caaa63f6c06ee8
sonods-sami <samysnds99@gmail.com>
2025-09-07 03:01:31 +0300

    update

--- src/app/_Component/Skeleton/OrderSkeleton.tsx
+++ src/app/_Component/Skeleton/OrderSkeleton.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function OrderSkeleton() {
   return (
  
-        <section className="p-6 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-4">
+        <section className="p-6 grid grid-cols-1   gap-4 mt-4">
              <Skeleton className="rounded-xl">
           <div className="h-[4rem] w-[270px] rounded-xl bg-default-300" />
         </Skeleton>
